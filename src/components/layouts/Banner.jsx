dfb35a1f5db512ed07d826001d58a5fd3da2523f
jaber-2004 <md9292463@gmail.com>
2025-12-16 21:27:28 +0600

    add

--- src/components/layouts/Banner.jsx
+++ src/components/layouts/Banner.jsx
@@ -5,18 +5,24 @@ import NextArrow from "../NextArrow";
 import PrevArrow from "../PrevArrow";
 
 const Banner = () => {
-   var settings = {
-     dots: false,
-     infinite: true,
-     speed: 500,
-     slidesToShow: 1,
-     slidesToScroll: 1,
-     nextArrow: <NextArrow />,
-     prevArrow: <PrevArrow />,
-   };
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 3000,
+    autoplaySpeed: 3000,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+  };
   return (
     <>
       <Slider {...settings}>
+        <Link>
+          <div className="bg-[url('/src/assets/banner.png')] py-[220px] bg-cover bg-no-repeat bg-center"></div>
+        </Link>
         <Link>
           <div className="bg-[url('/src/assets/banner.png')] py-[220px] bg-cover bg-no-repeat bg-center"></div>
         </Link>
